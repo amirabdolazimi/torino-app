22176bf75c67c4b5344d3c10ed45738c428a4a07
amirabdolazimi <amirabdolazimi.v@gmail.com>
2025-05-04 17:50:44 +0330

    bug fixed for next/image components

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <>
-      <div className="container mx-auto px-8 border-dashed border-t mt-16 border-[#00000040] lg:border-solid flex flex-col gap-y-9 lg:flex-row lg:justify-between">
+      <div className="container lg:max-w-6xl mx-auto px-8 border-dashed border-t mt-16 border-[#00000040] lg:border-solid flex flex-col gap-y-9 lg:flex-row lg:justify-between">
         <div className="flex justify-between  lg:gap-x-24 pt-5 lg:pt-5 lg:pb-7">
           <div className="flex flex-col gap-2.5 lg:gap-2">
             <h5 className="text-2xl font-semibold mb-2 ">تورینو</h5>
@@ -23,52 +23,52 @@ const Footer = () => {
 
         <div className="flex justify-between items-center lg:flex-col-reverse lg:items-end lg:justify-around">
           <div className="flex gap-y-2 gap-x-4 flex-wrap w-1/4 lg:flex-row lg:w-fit justify-evenly">
-            <div className="relative w-9 h-10 lg:w-16 lg:h-20">
+            <div className="relative -z-10 w-9 h-10 lg:w-16 lg:h-20">
               <Image
                 src="/images/img1.png"
                 fill
                 alt="aira Logo Validity....."
-                className="object-contain"
+                className="z-0"
               />
             </div>
-            <div className="relative w-9 h-10 lg:w-16 lg:h-20">
+            <div className="relative -z-10 w-9 h-10 lg:w-16 lg:h-20">
               <Image
                 src="/images/img2.png"
                 fill
                 alt="ecunion Logo Validity..."
-                className="object-contain"
+                className="z-0"
               />
             </div>
-            <div className="relative w-8 h-10 lg:w-16 lg:h-20">
+            <div className="relative -z-10 w-8 h-10 lg:w-16 lg:h-20">
               <Image
                 src="/images/img3.png"
                 fill
                 alt="passengers right Logo Validity"
-                className="object-contain"
+                className="z-0"
               />
             </div>
-            <div className="relative w-8 h-10 lg:w-20 lg:h-20">
+            <div className="relative -z-10 w-8 h-10 lg:w-20 lg:h-20">
               <Image
                 src="/images/img4.png"
                 fill
                 alt="samandehi Logo Validity"
-                className="object-contain"
+                className="z-0"
               />
             </div>
-            <div className="relative w-10 h-9 lg:w-16 lg:h-20">
+            <div className="relative -z-10 w-10 h-9 lg:w-16 lg:h-20">
               <Image
                 src="/images/img5.png"
                 fill
                 alt="state-airline Logo Validity"
-                className="object-contain"
+                className="z-0"
               />
             </div>
           </div>
-          <div className="flex flex-col gap-y-3">
-            <div className="relative w-24 h-8 lg:w-36 lg:h-11">
+          <div className="flex flex-col gap-y-3 items-end">
+            <div className="relative -z-10 w-24 h-8 lg:w-36 lg:h-11">
               <Image
-                className="self-end"
                 alt="Torino Logo"
+                className="z-0"
                 src="/images/logo.png"
                 fill
               />
@@ -81,9 +81,10 @@ const Footer = () => {
         </div>
         {/* Rights Reserved */}
       </div>
+
       <p className="text-xs font-light text-center py-2 lg:py-3 lg:text-base lg:font-medium broder border-t border-[#00000040] lg:border-[#00000033]">
-         کلیه حقوق این وب سایت متعلق به
-        <strong className="text-primary"> تورینو </strong> 
+        کلیه حقوق این وب سایت متعلق به
+        <strong className="text-primary"> تورینو </strong>
         میباشد.
       </p>
     </>
--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import { FiBell } from "react-icons/fi";
 import { IoMdAirplane } from "react-icons/io";
 import { MdCall, MdHome } from "react-icons/md";
@@ -8,93 +8,118 @@ import { SlMenu } from "react-icons/sl";
 import { useClickOutside } from "@/app/hooks/useClickOutSide";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import Image from "next/image";
 
 const Header = () => {
   const pathName = usePathname();
   const [openMenu, setOpenMenu] = useState<boolean>(false);
-  const ref = useRef(null);
+  const sidebarRef = useRef(null);
 
-  useClickOutside(ref, () => {
+  useClickOutside(sidebarRef, () => {
     setOpenMenu(false);
   });
 
   return (
-    <div className="relative">
-      <header className=" container mx-auto px-8 ">
-        <div className="flex justify-between items-center py-3">
-          <SlMenu
-            className="w-7 h-6 cursor-pointer"
-            onClick={() => setOpenMenu(true)}
+    <div className="shadow">
+      <Fragment>
+        <header className="lg:hidden container mx-auto sticky px-8 ">
+          <div className="flex justify-between relative items-center py-3">
+            <SlMenu
+              className="w-7 h-6 cursor-pointer"
+              onClick={() => setOpenMenu(true)}
+            />
+            <RxExit className="h-10 w-10" color="green" />
+          </div>
+          {/* SideBar */}
+          <Sidebar ref={sidebarRef} openMenu={openMenu} pathName={pathName} />
+          {/* BackDrop */}
+          {openMenu && (
+            <div
+              onClick={() => setOpenMenu(false)}
+              className="backdrop-blur-sm lg:hidden bg-black/20 opacity-100 transition-all ease-in-out duration-200 w-full h-full fixed inset-0 z-30"
+            ></div>
+          )}
+        </header>
+        {/* Hero Section */}
+        <div className="w-full relative -z-10 min-h-[120px] lg:min-h-[350px]">
+          <Image
+            alt="Torino Hero Section Banner"
+            src="/images/header-banner.png"
+            fill
+            className="z-0"
           />
-          <RxExit className="h-10 w-10" color="green" />
         </div>
-      </header>
-      {/* Sidebar */}
-      <div
-        ref={ref}
-        className={`fixed bg-white top-0  transform transition ease-in-out duration-200 right-0 py-8 px-3  w-1/2 z-40 h-full rounded-tl-xl ${
-          !openMenu && "translate-x-full"
-        }`}
-      >
-        <nav>
-          <ul className="flex flex-col gap-5 ">
-            <li>
-              <Link
-                href="/"
-                className={`text-xl flex justify-start items-center ${
-                  pathName == "/" ? "text-primary" : ""
-                }`}
-              >
-                <MdHome />
-                <span className="mr-2">صفحه اصلی</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className={`text-xl flex justify-start items-center ${
-                  pathName == "#" ? "text-primary" : ""
-                }`}
-              >
-                <IoMdAirplane />
-                <span className="mr-2">خدمات گردشگری</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className={`text-xl flex justify-start items-center ${
-                  pathName == "#" ? "text-primary" : ""
-                }`}
-              >
-                <FiBell />
-                <span className="mr-2">درباره ما</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className={`text-xl flex justify-start items-center ${
-                  pathName == "#" ? "text-primary" : ""
-                }`}
-              >
-                <MdCall />
-                <span className="mr-2">تماس با ما</span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      {/* backdrop */}
-      {openMenu && (
-        <div
-          onClick={() => setOpenMenu(false)}
-          className="backdrop-blur-sm bg-black/20 opacity-100 transition-all ease-in-out duration-200 w-full h-full fixed inset-0 z-30"
-        ></div>
-      )}
+      </Fragment>
+
+      {/* Desktop Menu */}
+      <Fragment></Fragment>
     </div>
   );
 };
 
 export default Header;
+
+interface ISidebar {
+  ref: React.RefObject<HTMLDivElement | null>;
+  openMenu: boolean;
+  pathName: string;
+}
+const Sidebar: React.FC<ISidebar> = ({ ref, openMenu, pathName }) => {
+  return (
+    <div
+      ref={ref}
+      className={`lg:hidden fixed bg-white top-0 transform h-full transition ease-in-out duration-200 right-0 py-8 px-3 w-1/2 z-40 rounded-tl-xl ${
+        !openMenu && "translate-x-full"
+      }`}
+    >
+      <nav>
+        <ul className="flex flex-col gap-5 ">
+          <li>
+            <Link
+              href="/"
+              className={`text-xl flex justify-start items-center ${
+                pathName == "/" ? "text-primary" : ""
+              }`}
+            >
+              <MdHome />
+              <span className="mr-2">صفحه اصلی</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/"
+              className={`text-xl flex justify-start items-center ${
+                pathName == "#" ? "text-primary" : ""
+              }`}
+            >
+              <IoMdAirplane />
+              <span className="mr-2">خدمات گردشگری</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/"
+              className={`text-xl flex justify-start items-center ${
+                pathName == "#" ? "text-primary" : ""
+              }`}
+            >
+              <FiBell />
+              <span className="mr-2">درباره ما</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/"
+              className={`text-xl flex justify-start items-center ${
+                pathName == "#" ? "text-primary" : ""
+              }`}
+            >
+              <MdCall />
+              <span className="mr-2">تماس با ما</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
